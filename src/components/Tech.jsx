d1623e6ebd184dbfc4aa893131ed6bef92f4a01d
Pranay Patle <pranayhpatle@gmail.com>
2024-02-18 23:52:44 +0530

    changes to tech section UI Improve

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -14,7 +14,7 @@ const Tech = () => {
       ".Net Core",
       "Angular 9",
     ],
-    tecnologies: ["AWS", "GCP", "Jenkins", "Docker", "Git",'Clerk','Prisma'],
+    tecnologies: ["AWS", "GCP", "Jenkins", "Docker", "Git", "Clerk", "Prisma"],
     misc: ["MSSQL", "MongoDB", "HTML5", "CSS", "ShadCN", "Tailwind CSS"],
   };
   return (
@@ -24,15 +24,15 @@ const Tech = () => {
     >
       <h2 className={`${styles.sectionHeadText} mb-11`}>Technologies</h2>
       {/* grid that divides in 2 row */}
-      <div className="grid grid-rows-2  gap-14">
+      <div className="flex flex-col  gap-10 ">
         {/* grid that divides in 2 col */}
-        <div className="grid md:grid-cols-2 gap-14	">
+        <div className="grid md:grid-cols-2 gap-10	">
           <div>
             <p className={styles.sectionSubText}>Programming Languages</p>
             <p className="mt-4 text-secondary sm:text-[18px] text-[14px] text-neutral-400">
               Languages I have mastered
             </p>
-            <div className="grid grid-cols-3 gap-2 justify-start mt-4">
+             <div className="grid md:grid-cols-3 grid-cols-2 gap-2 justify-start mt-4">
               {tech.languages.map((technology, index) => (
                 <div
                   className="p-6 border border-neutral-700 hover:border-white hover:cursor-pointer"
@@ -48,7 +48,7 @@ const Tech = () => {
             <p className="mt-4 text-secondary sm:text-[18px] text-[14px] text-neutral-400">
               Frameworks I love working with
             </p>
-            <div className="grid grid-cols-3 gap-2 justify-start mt-4">
+             <div className="grid md:grid-cols-3 grid-cols-2 gap-2 justify-start mt-4">
               {tech.framework.map((technology, index) => (
                 <div
                   className="p-6 border border-neutral-700 hover:border-white hover:cursor-pointer"
@@ -62,13 +62,13 @@ const Tech = () => {
         </div>
 
         {/* grid that divides in 2 col */}
-        <div className="grid md:grid-cols-2 gap-14	">
+        <div className="grid md:grid-cols-2 gap-10">
           <div>
             <p className={styles.sectionSubText}>Technologies</p>
             <p className="mt-4 text-secondary sm:text-[18px] text-[14px] text-neutral-400">
               Technologies I got a chance to interact with
             </p>
-            <div className="grid grid-cols-3 gap-2 justify-start mt-4">
+            <div className="grid md:grid-cols-3 grid-cols-2 gap-2 justify-start mt-4">
               {tech.tecnologies.map((technology, index) => (
                 <div
                   className="p-6 border border-neutral-700 hover:border-white hover:cursor-pointer "
@@ -84,7 +84,7 @@ const Tech = () => {
             <p className="mt-4 text-secondary sm:text-[18px] text-[14px] text-neutral-400">
               Stuff I worked with
             </p>
-            <div className="grid grid-cols-3 gap-2 justify-start mt-4">
+             <div className="grid md:grid-cols-3 grid-cols-2 gap-2 justify-start mt-4">
               {tech.misc.map((technology, index) => (
                 <div
                   className="p-6 border border-neutral-700 hover:border-white hover:cursor-pointer"
