687291becdee68a0fb620456a868a161f645e23e
Pranay Patle <pranayhpatle@gmail.com>
2024-02-19 23:39:23 +0530

    Added Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from '@vercel/analytics/react';
 
 import App from "./App";
 import "./index.css";
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Toaster />
+    <Analytics/>
   </React.StrictMode>
 );
