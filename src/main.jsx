b4d5e12a9934231b6fde9fcdce4693954232d72f
Pranay Patle <pranayhpatle@gmail.com>
2024-02-21 01:21:51 +0530

    Revert "cursor changes"
    This reverts commit 76d927763826b94b22b9b726429d32c9f6177975.

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from '@vercel/analytics/react';
 
 import App from "./App";
 import "./index.css";
 import { Toaster } from "./components/ui/toaster";
-import AnimatedCursor from "react-animated-cursor";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Toaster />
-    <AnimatedCursor
-      innerSize={4}
-      outerSize={20}
-      innerScale={1}
-      outerScale={1.5}
-      outerAlpha={0}
-      hasBlendMode={false}
-      innerStyle={{
-        backgroundColor: "var(--cursor-color)",
-      }}
-      outerStyle={{
-        border: "3px solid #595959",
-      }}
-    />
-    <Analytics />
+    <Analytics/>
   </React.StrictMode>
 );
