cea8e259a301e2dff2ec12c04834651699ca4934
Pranay Patle <pranayhpatle@gmail.com>
2024-02-25 00:51:40 +0530

    editor image update

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import Telehealth from "./telehealth.png";
 import Dashboard from "./dashboard.png";
 import TCS from "./tcs_logo.png";
 import KrushiSarathi from './krushisarathi.png'
-import Editor from './editor.png'
+import Editor from './editor.jpg'
 import Ticket from './ticket.png'
 import NoImage from './no-image.svg'
 import logo from './logo.png'
