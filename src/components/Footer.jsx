90523078745fc289af4e6125ba558ece6c41f822
Pranay Patle <pranayhpatle@gmail.com>
2024-03-27 19:28:52 +0530

    added link to project repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,3 @@
-import { SectionWrapper } from "@/hoc";
-import { styles } from "@/styles";
 import React from "react";
 
 const Footer = () => {
@@ -8,17 +6,28 @@ const Footer = () => {
       <footer className="max-w-7xl mx-auto sm:px-16 px-6 py-2">
         <section className="flex md:flex-row flex-col justify-between items-center space-y-2">
           <div>
-            <p className={"sm:text-[16px] text-[12px] text-secondary uppercase tracking-wider"}>Work in Progress...</p>
-          </div>
-           <div className="block md:hidden">
-            <p className={"sm:text-[16px] text-[12px] text-secondary uppercase tracking-wider"}>Feedback Appreciated</p>
+            <a href="https://github.com/patlepranay/developer_portfolio" target="_blank" rel="noreferrer">
+              <p
+                className={
+                  "sm:text-[16px] text-[12px] text-secondary uppercase tracking-wider underline"
+                }
+              >
+                Portfolio Repository
+              </p>
+            </a>
           </div>
           <div>
-            <h2 className={"sm:text-[16px] text-[12px] text-secondary uppercase tracking-wider"}>Designed and Developed by Pranay</h2>
+            <h2
+              className={
+                "sm:text-[16px] text-[12px] text-secondary uppercase tracking-wider"
+              }
+            >
+              Designed and Developed by Pranay
+            </h2>
           </div>
         </section>
       </footer>
     </>
   );
 };
-export default Footer
+export default Footer;
